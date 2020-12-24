7d8c054023e2e328498b7bbb633d90ef73248f51
norayda <norayda.sims@gmail.com>
2020-12-24 18:48:09 +0100

    nouveau comit

--- data.py
+++ data.py
@@ -2,13 +2,42 @@
 #
 import json
 import requests
+import os
+
+def merge_dicts(dict1,dict2):
+    dico = {}
+    for x in dict1.keys():
+        dico[x] = []
+        if (type(dict1[x]) is list):
+            dico[x].extend(dict1[x])
+            dico[x].extend(dict2[x])
+        else:
+            dico[x].append(dict1[x])
+            dico[x].append(dict2[x])
+    return dico
 
 
 def saving_fichier_json(data: dict, filename: str = "fichier.json"):
-    with open("pole_emploi/" + filename, "w") as file:
+    donnees = json.loads(json.dumps(data))
+    if not (os.path.isfile("data/" + filename)):
+        with open("data/" + filename, "w") as file:
+            json.dump(donnees, file)
+            file.close()
+
+    else :
+        with open("data/" + filename, "r") as file:
+            fichier = json.load(file)
+            file.close()
+        fichier = merge_dicts(fichier, donnees) #on fusionne les anciennes données contenues dans le fichier et les nouvelles données
+        with open("data/" + filename, "w") as file :
+            json.dump(fichier,file)
+            file.close()
+
+
+def saving_file_json (data,filename = "fichier.json"):
+    with open(filename, "w") as file:
         json.dump(data, file)
         file.close()
-
 
 def load_api(client_id, client_secret, scope):
     url = "https://entreprise.pole-emploi.fr/connexion/oauth2/access_token?realm=/partenaire"
@@ -41,3 +70,9 @@ def load_api_data(token):
     response = requests.request("GET", url, headers=headers, data=payload)
 
     return response
+
+def extract_salary(data) :
+    #renvoie la liste des offres qui ont renseigné le salaire sous forme de dictionnaire
+    data["salaire"].fillna(0, inplace=True)
+    return data.query('salaire != 0') ##renvoie uniquement les ligne qui possèdent une information sur le salaire
+
